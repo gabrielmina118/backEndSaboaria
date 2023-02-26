c00bd00bd6d8b75670222910fb2ef53ec6a1e543
gabrielmina118 <gabrielmina118@gmail.com>
2023-02-26 17:40:09 -0300

    salva nois leo

--- src/controllers/product/ProductController.ts
+++ src/controllers/product/ProductController.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
+import { categoryDb } from "../../modelDB/Category";
 import { productDb } from "../../modelDB/Products";
-
 
 class ProductController {
   public static async get(req: Request, res: Response) {
     try {
-      const allCategories = await productDb.find();
-      res.status(200).json(allCategories);
+      const allProducts = await productDb.find();
+      const allCategories = await categoryDb.find();
+
+      const concat = allCategories.map((category) => {
+        console.log("category", category);
+      });
+
+      
+      // res.status(200).json(concat);
     } catch (error) {
       console.log(error);
     }
