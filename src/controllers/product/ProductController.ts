5456b4ad75a2ebf2af444100a5c98dca2e7183da
gabrielmina118 <gabrielmina118@gmail.com>
2023-02-26 20:21:55 -0300

    feat(api): api produtos

--- src/controllers/product/ProductController.ts
+++ src/controllers/product/ProductController.ts
@@ -1,17 +1,31 @@
 import { Request, Response } from "express";
 import { categoryDb } from "../../modelDB/Category";
 import { productDb } from "../../modelDB/Products";
+import { ICategories ,Products} from "./interface/ICategories";
 
 class ProductController {
   public static async get(req: Request, res: Response) {
     try {
       const allCategories = await categoryDb.find();
-     
-      allCategories.map(async(category)=>{
-        const product = await productDb.find({categoria_id:category._id.toString()});
-        console.log(product)
-      }) 
-    
+      const allProducts = await productDb.find();
+      
+      const allcategoriesObject: Record<string, ICategories> = {
+        semCategoria: { _id: undefined, nome: "produtoSemCategoria", produtos: [] },
+      };
+
+      allCategories.map((category) => {
+        allcategoriesObject[category._id.toString()] = {
+          _id: category._id.toString(),
+          nome: category.nome,
+          produtos: [],
+        };
+      });
+
+      allProducts.forEach(function (product) {
+        allcategoriesObject[product.categoria_id || "semCategoria"].produtos.push(product._doc);
+      });
+
+      res.send(Object.values(allcategoriesObject))
     } catch (error) {
       console.log(error);
     }
